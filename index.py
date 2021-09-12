1f26766d1f19e92a834137c8dc98b67e055b6d82
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-09-12 10:44:57 +0700

    - bs

--- index.py
+++ index.py
@@ -13,22 +13,22 @@ y_obj = 430
 lompat = False
 jumpCount = 10
 
-music = pygame.mixer.music.load('assets/backsound.mp3')
+music = pygame.mixer.music.load('asset/backsound.mp3')
 pygame.mixer.music.play()
 
-bg = pygame.image.load('assets/bg.jpg')
+bg = pygame.image.load('asset/bg.jpg')
 bg = pygame.transform.scale(bg, (800,600))
 
-objek = pygame.image.load('assets/ghost.png')
+objek = pygame.image.load('asset/ghost.png')
 objek = pygame.transform.scale(objek, (150,150))
 
-roket = pygame.image.load('assets/rocket.png')
+roket = pygame.image.load('asset/rocket.png')
 roket = pygame.transform.scale(roket, (100,60))
 
-api = pygame.image.load('assets/fire.png')
+api = pygame.image.load('asset/fire.png')
 api = pygame.transform.scale(api, (180,230))
 
-go = pygame.image.load('assets/go.png')
+go = pygame.image.load('asset/go.png')
 go = pygame.transform.scale(go, (400,250))
 
 font = pygame.font.SysFont('comicsans',30,True)
